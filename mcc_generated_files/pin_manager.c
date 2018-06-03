80237896ea314b6f71eccbcd994ff48be69f3ee2
R B <726f64>
2018-06-03 20:05:00 +0100

    Added mcc files

---
+++ mcc_generated_files/pin_manager.c
@@ -0,0 +1,110 @@
+/**
+  Generated Pin Manager File
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    pin_manager.c
+
+  Summary:
+    This is the Pin Manager file generated using MPLAB(c) Code Configurator
+
+  Description:
+    This header file provides implementations for pin APIs for all pins selected in the GUI.
+    Generation Information :
+        Product Revision  :  MPLAB(c) Code Configurator - 4.26
+        Device            :  PIC16F18857
+        Driver Version    :  1.02
+    The generated drivers are tested against the following:
+        Compiler          :  XC8 1.35
+        MPLAB             :  MPLAB X 3.40
+
+    Copyright (c) 2013 - 2015 released Microchip Technology Inc.  All rights reserved.
+
+    Microchip licenses to you the right to use, modify, copy and distribute
+    Software only when embedded on a Microchip microcontroller or digital signal
+    controller that is integrated into your product or third party product
+    (pursuant to the sublicense terms in the accompanying license agreement).
+
+    You should refer to the license agreement accompanying this Software for
+    additional information regarding your rights and obligations.
+
+    SOFTWARE AND DOCUMENTATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+    EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION, ANY WARRANTY OF
+    MERCHANTABILITY, TITLE, NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE.
+    IN NO EVENT SHALL MICROCHIP OR ITS LICENSORS BE LIABLE OR OBLIGATED UNDER
+    CONTRACT, NEGLIGENCE, STRICT LIABILITY, CONTRIBUTION, BREACH OF WARRANTY, OR
+    OTHER LEGAL EQUITABLE THEORY ANY DIRECT OR INDIRECT DAMAGES OR EXPENSES
+    INCLUDING BUT NOT LIMITED TO ANY INCIDENTAL, SPECIAL, INDIRECT, PUNITIVE OR
+    CONSEQUENTIAL DAMAGES, LOST PROFITS OR LOST DATA, COST OF PROCUREMENT OF
+    SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
+    (INCLUDING BUT NOT LIMITED TO ANY DEFENSE THEREOF), OR OTHER SIMILAR COSTS.
+
+*/
+
+#include <xc.h>
+#include "pin_manager.h"
+#include "stdbool.h"
+
+
+
+void PIN_MANAGER_Initialize(void)
+{
+    /**
+    LATx registers
+    */   
+    LATA = 0x00;    
+    LATB = 0x00;    
+    LATC = 0x00;    
+
+    /**
+    TRISx registers
+    */    
+    TRISA = 0x00;
+    TRISB = 0x00;
+    TRISC = 0x27;
+
+    /**
+    ANSELx registers
+    */   
+    ANSELC = 0x00;
+    ANSELB = 0x00;
+    ANSELA = 0x00;
+    
+    /**
+    WPUx registers
+    */ 
+    WPUB = 0xF0;
+    WPUA = 0x3F;
+    WPUC = 0x00;
+
+    /**
+    ODx registers
+    */
+    ODCONA = 0x00;
+    ODCONB = 0x00;
+    ODCONC = 0x00;
+
+    //PPS
+    RB3PPS=0x04;        //the CLC4 output
+    RB2PPS=0x02;        //the CLC2 output
+    RA1PPS=0x0E;        //PWM6 op
+    RB1PPS=0x14;        //SCLK
+    RB0PPS=0x15;        //SPI1 ??
+    RB4PPS=0x18;        //TMR0 for testing
+    
+    SSP2DATPPS=0x10;
+    SSP2CLKPPS=0x11;
+    SSP2SSPPS=0x12;
+    
+}       
+
+void PIN_MANAGER_IOC(void)
+{   
+
+}
+
+/**
+ End of File
+*/
